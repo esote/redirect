bf07a8d4cd33be3e55218124c1e9274766a36bbe
esote <esote.net@gmail.com>
2019-11-20 11:33:32 -0600

    Fix localhost redirect from reverse proxy

--- website-redirect.go
+++ website-redirect.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/esote/openshim2"
 )
@@ -16,14 +17,26 @@ func init() {
 
 func redirect(w http.ResponseWriter, r *http.Request) {
 	host, err := url.Parse("http://" + r.Host)
-
-	if err != nil || host.Hostname() == "" {
+	if err != nil {
 		return
 	}
 
+	hostname := host.Hostname()
+	if hostname == "" {
+		return
+	}
+
+	/* Hostname will be localhost, since it comes from the reverse proxy. */
+	i := strings.LastIndex(hostname, "localhost")
+	if i == -1 {
+		return
+	}
+
+	end := strings.Replace(hostname[i:], "localhost", "esote.net", 1)
+
 	u := &url.URL{
 		Scheme: "https",
-		Host:   host.Hostname() + ":443",
+		Host:   hostname[:i] + end + ":443",
 	}
 
 	http.Redirect(w, r, u.String(), http.StatusPermanentRedirect)
